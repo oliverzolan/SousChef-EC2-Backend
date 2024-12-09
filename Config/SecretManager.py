0c24aab512dca652a6f957b8537d306869ba3ccf
Sutter-Reynolds <sutterreynolds@gmail.com>
2024-12-09 03:39:47 +0000

    Secret Manager

---
+++ Config/SecretManager.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+import os
+from dotenv import load_dotenv
+
+def get_secret(secret_name, region_name):
+    """
+    Retrieve a secret from AWS Secrets Manager.
+    """
+    client = boto3.client("secretsmanager", region_name=region_name)
+    try:
+        response = client.get_secret_value(SecretId=secret_name)
+        if 'SecretString' in response:
+            return json.loads(response['SecretString'])
+        else:
+            raise Exception("SecretBinary is not supported")
+    except Exception as e:
+        print(f"Error retrieving secret {secret_name}: {e}")
+        raise e
+
