d68c90cff51f707b0de0d63b41dda83c081fa709
Sutter-Reynolds <sutterreynolds@gmail.com>
2024-12-11 09:15:36 +0000

    Made more component based

--- app.py
+++ app.py
@@ -7,24 +7,27 @@ from Controller.RecipeController import recipe_blueprint
 from Controller.UserController import user_blueprint
 from Config.Fb import initialize_firebase
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-initialize_firebase()
+    initialize_firebase()
 
-# Register Blueprints
-app.register_blueprint(pantry_blueprint, url_prefix='/pantry')
-app.register_blueprint(recipe_blueprint, url_prefix='/recipes')
-app.register_blueprint(user_blueprint, url_prefix='/users')
-app.register_blueprint(ingredients_blueprint, url_prefix='/ingredients')
+    app.register_blueprint(pantry_blueprint, url_prefix='/pantry')
+    app.register_blueprint(recipe_blueprint, url_prefix='/recipes')
+    app.register_blueprint(user_blueprint, url_prefix='/users')
+    app.register_blueprint(ingredients_blueprint, url_prefix='/ingredients')
 
-load_dotenv()
+    return app
 
-app.run(
-    host="0.0.0.0",
-    port=5000,
-    ssl_context=(
-        os.getenv("SSL_CERT_PATH"),
-        os.getenv("SSL_KEY_PATH")
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        ssl_context=(
+            os.getenv("SSL_CERT_PATH"),
+            os.getenv("SSL_KEY_PATH")
+        )
     )
-)
 
