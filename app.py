346d71f76bbaa88475a2bd6d40f7927b5e0923ad
Sutter-Reynolds <sutterreynolds@gmail.com>
2025-04-01 00:35:54 +0000

    Removed unneeded

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from Controller.IngredientsController import ingredients_blueprint
-from Controller.PantryController import pantry_blueprint
-from Controller.RecipeController import recipe_blueprint
+# from Controller.PantryController import pantry_blueprint
+from Controller.RecipeController import recipes_blueprint
 from Controller.UserController import user_blueprint
 from Config.Fb import initialize_firebase
 
@@ -14,7 +14,7 @@ app = Flask(__name__)
 initialize_firebase()
 
 # app.register_blueprint(pantry_blueprint, url_prefix='/pantry')
-app.register_blueprint(recipe_blueprint, url_prefix='/recipes')
+app.register_blueprint(recipes_blueprint, url_prefix='/recipes')
 app.register_blueprint(user_blueprint, url_prefix='/users')
 app.register_blueprint(ingredients_blueprint, url_prefix='/ingredients')
 
