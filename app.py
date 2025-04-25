737deefc49df7bfd055747da757b297014a99613
Sutter-Reynolds <sutterreynolds@gmail.com>
2025-04-25 07:40:20 +0000

    Adding reports

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from firebase_admin import credentials, auth
 # from Controller.PantryController import pantry_blueprint
 from Controller.UserIngredientsController import user_ingredients_blueprint
 from Controller.InternalIngredientsController import internal_ingredients_blueprint
+from Controller.ReportsController import reports_blueprint
 from Controller.RecipeController import recipes_blueprint
 from Controller.UserController import user_blueprint
+
 from Config.Fb import initialize_firebase
 
 
@@ -21,6 +23,7 @@ app.register_blueprint(recipes_blueprint, url_prefix='/recipes')
 app.register_blueprint(user_blueprint, url_prefix='/users')
 app.register_blueprint(user_ingredients_blueprint, url_prefix='/user_ingredients')
 app.register_blueprint(internal_ingredients_blueprint, url_prefix='/internal_ingredients')
+app.register_blueprint(reports_blueprint, url_prefix='/reports')
 
 # Setup for python3
 if __name__ == '__main__':
